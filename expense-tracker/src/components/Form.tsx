d4c7e3361e815c79f253065db8883e6b8ffed815
zeeshan <zeeshan.tariq@quantum-h.com>
2024-08-07 22:42:23 +0500

    accessing input fields , using ref hook

--- expense-tracker/src/components/Form.tsx
+++ expense-tracker/src/components/Form.tsx
@@ -1,21 +1,33 @@
-import {FormEvent} from "react";
+import {FormEvent, useRef} from "react";
 
 
 const Form  = ()=>{
 
+    const nameRef = useRef<HTMLInputElement>(null);
+    const ageRef = useRef<HTMLInputElement>(null);
+    const person = {
+        name : "",
+        age:0
+    }
+
     const handleSubmit = (event:FormEvent)=>{
         event.preventDefault();
-        console.log("submitted");
+        if (nameRef.current)
+            person.name =(nameRef.current.value);
+        if (ageRef.current)
+            person.age =parseInt(ageRef.current.value);
+
+        console.log(person)
     }
     return (
         <form onSubmit={handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input type="text" className="form-control" id="name"/>
+                <input ref={nameRef} type="text" className="form-control" id="name"/>
             </div>
             <div className="mb-3">
                 <label htmlFor="age" className="form-label">Age</label>
-                <input type="number" className="form-control" id="age"/>
+                <input ref={ageRef} type="number" className="form-control" id="age"/>
             </div>
 
             <button className="btn btn-primary">Submit</button>

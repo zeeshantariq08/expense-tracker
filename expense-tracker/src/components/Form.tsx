b870b7688419e7d669ef165eef5285610f7112cb
zeeshan <zeeshan.tariq@quantum-h.com>
2024-08-08 00:12:55 +0500

    accessing input fields , using state hook

--- expense-tracker/src/components/Form.tsx
+++ expense-tracker/src/components/Form.tsx
@@ -1,21 +1,26 @@
-import {FormEvent, useRef} from "react";
+import {FormEvent, useRef, useState} from "react";
 
 
 const Form  = ()=>{
 
-    const nameRef = useRef<HTMLInputElement>(null);
-    const ageRef = useRef<HTMLInputElement>(null);
-    const person = {
-        name : "",
+    const [person, setPerson] = useState({
+        name: "",
         age:0
-    }
+    })
+
+    // const nameRef = useRef<HTMLInputElement>(null);
+    // const ageRef = useRef<HTMLInputElement>(null);
+    // const person = {
+    //     name : "",
+    //     age:0
+    // }
 
     const handleSubmit = (event:FormEvent)=>{
         event.preventDefault();
-        if (nameRef.current)
-            person.name =(nameRef.current.value);
-        if (ageRef.current)
-            person.age =parseInt(ageRef.current.value);
+        // if (nameRef.current)
+        //     person.name =(nameRef.current.value);
+        // if (ageRef.current)
+        //     person.age =parseInt(ageRef.current.value);
 
         console.log(person)
     }
@@ -23,11 +28,14 @@ const Form  = ()=>{
         <form onSubmit={handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input ref={nameRef} type="text" className="form-control" id="name"/>
+                <input value={person.name} onChange={(event) => setPerson({...person, name: event.target.value})}
+                       type="text" className="form-control" id="name"/>
             </div>
             <div className="mb-3">
                 <label htmlFor="age" className="form-label">Age</label>
-                <input ref={ageRef} type="number" className="form-control" id="age"/>
+                <input value={person.age}
+                       onChange={(event) => setPerson({...person, age: parseInt(event.target.value)})} type="number"
+                       className="form-control" id="age"/>
             </div>
 
             <button className="btn btn-primary">Submit</button>

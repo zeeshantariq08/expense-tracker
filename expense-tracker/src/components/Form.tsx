94199707194549ddf329986f84e424fea982bda5
zeeshan <zeeshan.tariq@quantum-h.com>
2024-08-07 22:23:34 +0500

    handle submit button

--- expense-tracker/src/components/Form.tsx
+++ expense-tracker/src/components/Form.tsx
@@ -1,15 +1,21 @@
+import {FormEvent} from "react";
 
 
 const Form  = ()=>{
+
+    const handleSubmit = (event:FormEvent)=>{
+        event.preventDefault();
+        console.log("submitted");
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
                 <input type="text" className="form-control" id="name"/>
             </div>
             <div className="mb-3">
-                <label htmlFor="amount" className="form-label">Amount</label>
-                <input type="number" className="form-control" id="amount"/>
+                <label htmlFor="age" className="form-label">Age</label>
+                <input type="number" className="form-control" id="age"/>
             </div>
 
             <button className="btn btn-primary">Submit</button>

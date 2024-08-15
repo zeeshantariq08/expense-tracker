6b5df5c8816149841c56dd262e2b7c2ae008a1b2
zeeshan <zeeshan.tariq@quantum-h.com>
2024-08-15 23:39:08 +0500

    applying validation to the forms

--- expense-tracker/src/components/Form.tsx
+++ expense-tracker/src/components/Form.tsx
@@ -2,9 +2,13 @@ import {FormEvent, useRef, useState} from "react";
 
 import {FieldValues, useForm} from "react-hook-form";
 
+interface FormData {
+    name: string
+    age: number
+}
 
-const Form  = ()=>{
-    const {register, handleSubmit} = useForm();
+const Form = () => {
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
 
     const onSubmit = (data: FieldValues) => {
         console.log(data)
@@ -34,8 +38,11 @@ const Form  = ()=>{
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input {...register('name')}
+                <input {...register('name', {required: true, minLength: 4})}
                        type="text" className="form-control" id="name"/>
+                {errors.name?.type == 'required' && <p className="text-danger">The name field is required</p>}
+                {errors.name?.type == 'minLength' &&
+                    <p className="text-danger">The name must be at least 4 characters</p>}
             </div>
             <div className="mb-3">
                 <label htmlFor="age" className="form-label">Age</label>
